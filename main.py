7b3535aad57a0905748d851b3830709d1ba3077b
batuhan6 <https://gitlab.com/batuhan6/myfirstgitlab.git>
2022-01-20 14:07:16 +0200

    x

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Pathlib import Path
+from pathlib import Path
 import requests
 import json
 import os
@@ -11,4 +11,27 @@ except FileExistsError:
     pass
 
 while True:
-    
+    endpoint = input("Lutfen api endpointini giriniz! Ya da cikmak icin exit yaziniz: ")
+    connect_api = requests.get(f"https://jsonplaceholder.typicode.com/{endpoint}")
+
+    if connect_api.status_code == 200:
+        data = connect_api.json()
+        try:
+            with open(f"{jsons_dir}/{endpoint}.json","r") as f:
+                answer = input("Bu dosya zaten mevcut. Uzerine mi yazilsin mevcut dosyaya ekleme mi yapilsin?     uzerine/ekleme: ")
+                if answer == 'uzerine':
+                    with open(f"{jsons_dir}/{endpoint}.json",'w') as f:
+                        json.dump(data, f, sort_keys=True, indent=4)
+                else:
+                    with open(f"{jsons_dir}/{endpoint}.json", 'a') as f:
+                        json.dump(data, f, sort_keys=True, indent=4)
+        except FileNotFoundError:
+            with open(f"{jsons_dir}/{endpoint}.json",'w') as f:
+                json.dump(data, f, sort_keys=True, indent=4)
+
+    elif endpoint == 'exit':
+        break
+
+    else:
+        print("Hatali endpoint girdin.")
+        print("x")
